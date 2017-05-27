c8c848d364b88f5fc0dc6e4c23cabb606aaf7798
David Jegat <david.jegat@knplabs.com>
2017-05-27 10:29:28 +0200

    Add a synchronous event dispatcher

--- README.md
+++ README.md
@@ -1,5 +1,53 @@
-Theatre Event Dispatcher
+Theatre Events
 ========================
 
 This library is a part of the **Theatre** module. You can use it ase is or with
 a complete **Theatre** installation.
+
+## About
+
+**Theatre Events** is a short but complete implementation of a standard `ÈventDispatcher` and
+`EventBroadcaster`. It can be use as a standalone module in any project to start
+helping you building robust events system.
+
+## Installation
+
+```
+npm install @theatre/events --save
+```
+
+## The EventDispatcher
+
+### Synchronous
+
+```javascript
+import {SynchronousEventDispatcher} from '@theatre/events';
+
+let dispatcher = new SynchronousEventDispatcher();
+
+// Register also supports symbols
+dispatcher.addEventListener('My Event', (myPayload) => {
+    console.log(myPayload);
+});
+
+// This command will log "{name: 'test'}"
+dispatcher.dispatch('My Event', {name: 'test'});
+```
+
+### Asynchronous
+
+```javascript
+import {AsynchronousEventDispatcher} from '@theatre/events';
+
+let dispatcher = new AsynchronousEventDispatcher();
+
+dispatcher.addEventListener('send email', (emailPayload) => {
+    sendMail(emailPayload);
+});
+
+// The dispatch method will not be blocking, so you can send your email
+// and do other stuff during the mail is processed
+dispatcher.dispatch('send email', {
+    subject: 'Do you heard about theatre components ?',
+}).then(d => console.log('The email has been sent')).catch(console.error);
+```
--- src/event-dispatcher/asynchronous-event-dispatcher.ts
+++ src/event-dispatcher/asynchronous-event-dispatcher.ts
@@ -1,5 +1,5 @@
 import {default as EventDispatcher, EventTarget} from './../event-dispatcher';
-import {default as EventListener} from './../event-listener';
+import {default as EventListener, NORMAL_PRIORITY} from './../event-listener';
 
 /**
  * An asynchronous implementation of an event dispatcher.
@@ -11,5 +11,84 @@ export default class AsynchronousEventDispatcher implements EventDispatcher
     constructor()
     {
         this.eventListeners = new Map();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    dispatch<T>(name: EventTarget, payload?: T): Promise<void[]>
+    {
+        let promises = [];
+
+        if (!this.eventListeners.has(name)) {
+            return Promise.all(promises);
+        }
+
+        let priorities: number[] = [];
+        this.eventListeners.get(name).forEach((v, k) => {priorities.push(k)});
+
+        priorities.sort().map(priority => this.eventListeners.get(name).get(priority).map(l => {
+            promises.push(l(payload))
+        }));
+
+        return Promise.all(promises);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    addEventListener(name: EventTarget, listener: EventListener): void
+    {
+        listener.priority = listener.priority || NORMAL_PRIORITY;
+
+        if (!this.eventListeners.has(name)) {
+            this.eventListeners.set(name, new Map());
+        }
+
+        if (!this.eventListeners.get(name).has(listener.priority)) {
+            this.eventListeners.get(name).set(listener.priority, []);
+        }
+
+        this.eventListeners.get(name).get(listener.priority).push(listener);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    removeEventListener(name: EventTarget, listener: EventListener): void
+    {
+        if (!this.eventListeners.get(name)) {
+            return;
+        }
+
+        this.eventListeners.get(name).forEach((stack, priority) => {
+            stack.map((callback, index) => {
+                if (callback !== listener) {
+                    return;
+                }
+
+                stack.splice(index, 1);
+            });
+
+            if (!stack.length) {
+                this.eventListeners.get(name).delete(priority);
+            }
+        });
+
+        if (!this.eventListeners.get(name).size) {
+            this.eventListeners.delete(name);
+        }
+    }
+
+    /**
+     * {inheritdoc}
+     */
+    clearEventListeners(name?: EventTarget): void
+    {
+        if (!name) {
+            this.eventListeners.clear();
+        }
+
+        this.eventListeners.has(name) && this.eventListeners.delete(name);
     }
 }
